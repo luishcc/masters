eb2093159c4e50f7e49d0b61260a357885cd6d18
luis <lh.carnevale@gmail.com>
2019-03-22 23:07:58 -0300

    ~

--- compCien/matrix.f90
+++ compCien/matrix.f90
@@ -8,7 +8,7 @@ real::dt, t1, t2, sumdt
 
 open (10, file='result.txt')
 
-num = 10000   
+num = 10  
  
 allocate (a(1:num,1:num))
 allocate (x(1:num))
@@ -16,7 +16,11 @@ allocate (b(1:num))
 
 
 sumdt=0
-do cont=1, 100
+do cont=1, 10
+
+t1=0
+t2=0
+dt = 0
 
 do i=1, num
   b(i)=0.0
